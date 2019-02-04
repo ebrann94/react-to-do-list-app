7ab8dcdbb5164cb8fa1784567aa91faedd7102b1
Ethan Brann <ethan.brann94@gmail.com>
2019-02-04 15:35:38 +0000

    Ready for heroku

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, '../dist');
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server running on port 3000');
 });
