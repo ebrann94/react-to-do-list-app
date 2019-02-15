d3ec6ff462a806014b67ad1bf9eba6e05c921832
Ethan Brann <ethan.brann94@gmail.com>
2019-02-15 19:57:37 +0000

    Added README file

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     ],
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'dist')
     }
 };
 
